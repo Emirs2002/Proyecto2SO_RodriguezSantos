9f565a8aceb41b06374bc8139e536626ad350719
Emily Rodriguez <em.rodriguez@correo.unimet.edu.ve>
2023-10-22 17:10:09 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author emirs
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello isaac");
+    }
+    
+}
