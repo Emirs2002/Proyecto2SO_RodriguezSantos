4c12489c7ca678d682b7be16f1445a362dc13ab5
Emily Rodriguez <em.rodriguez@correo.unimet.edu.ve>
2023-10-23 12:38:17 -0400

    atributos proceso

--- src/sistema/Process.java
+++ src/sistema/Process.java
@@ -1,9 +1,33 @@
 package sistema;
 
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author emirs
  */
 public class Process {
+    private int id;
+    private int prioridad; //1,2,3
+    private int counter;//max 8
+    public ImageIcon imagen;
+    private int abilities; //60%
+    private int lifePoints;//70%
+    private int strenght; //50%
+    private int agility; //40%
+    private static int processCounter;
+
+    public Process(int prioridad, ImageIcon imagen, int abilities, int lifePoints, int strenght, int agility) {
+        this.id = ++Process.processCounter;
+        this.prioridad = prioridad;
+        this.imagen = imagen;
+        this.abilities = abilities;
+        this.lifePoints = lifePoints;
+        this.strenght = strenght;
+        this.agility = agility;
+    }
+    
+    
     
 }
+
--- src/test/Main.java
+++ src/test/Main.java
@@ -10,7 +10,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello isaac");
+        String[] zeldaNamesArr = {"Link", "Zelda", "Ganondorf", "Impa", "Mipha", "Daruk", "Kass", "Riju","Sidon", "Yunobo"};
+//        String[] zeldaimagesArr = {"Link", "Zelda", "Ganondorf", "Impa", "Mipha", "Daruk", "Kass", "Riju","Sidon", "Yunobo"};
     }
     
 }
